d267dbc5af779ef6f639d1f2d264a232310b03b1
Jared Beck <jared@jaredbeck.com>
2018-12-07 22:43:59 -0500

    Consolidating Session modules: Params

--- lib/authlogic.rb
+++ lib/authlogic.rb
@@ -43,7 +43,6 @@ path = File.dirname(__FILE__) + "/authlogic/"
   "session/magic_column/assigns_last_request_at",
   "session/magic_columns",
   "session/magic_states",
-  "session/params",
   "session/password",
   "session/perishable_token",
   "session/persistence",
